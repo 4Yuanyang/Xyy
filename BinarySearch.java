55f4bb9c4b56ad2e1dc859060ee3298a2bfbf107
xyy <1823809284@qq.com>
2024-12-01 15:28:08 +0800

    add file: BinarySearch.java & HelloWorld.java

---
+++ BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+	public static void main(String[] args) {
+		var list = Arrays.asList(new int[]{1,2,3,4,5,9,19});
+		int l = 0, r = list.size - 1, mid;
+		while(l < r) {
+			mid = l + r >> 1;
+			if(list.get(mid) >= 4) r = mid;
+			else l = mid + 1;
+		}	
+		System.out.println(r);
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *@author xyy
+ *@date 2024/11/20
+ *@description 这是一个输出helloworld的简单java程序
+*/
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+		System.out.print("haha\txyy"); 
+		
+	}
+
+}
