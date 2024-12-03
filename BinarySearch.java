f1363acf443571c05b08ba6fb7e22c1c652959c3
xyy <1823809284@qq.com>
2024-12-03 09:45:16 +0800

    modify file: BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,12 +1,13 @@
 public class BinarySearch {
 	public static void main(String[] args) {
-		var list = Arrays.asList(new int[]{1,2,3,4,5,9,19});
-		int l = 0, r = list.size - 1, mid;
+		var list = Arrays.asList(1,2,3,4,5,9,19);
+		int l = 0, r = list.size() - 1, mid;
 		while(l < r) {
 			mid = l + r >> 1;
 			if(list.get(mid) >= 4) r = mid;
 			else l = mid + 1;
 		}	
 		System.out.println(r);
+		System.out.println(list.get(r));
 	}
 }
